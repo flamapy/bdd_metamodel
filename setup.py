9ae40252991a0d28096639a3a37ffe54f20ecb15
Jose A. Galndo <jagalindo@us.es>
2026-02-18 13:05:33 +0100

    bump: version bump to include all wheels

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ dev_requirements = read_requirements("requirements-dev.txt")
 
 setuptools.setup(
     name="flamapy-bdd",
-    version="2.1.0.dev1",
+    version="2.1.0.dev2",
     author="Flamapy",
     author_email="flamapy@us.es",
     description="bdd-plugin for the automated analysis of feature models",
